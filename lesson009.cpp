a201c1f6786917f16e098126c2abe55cd83c786c
Mark Peters <mapeters101@earthlink.net>
2024-12-01 16:45:57 -0500

    Work on lesson009 to 10:15

---
+++ lesson009.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+/*
+int main() {
+	int Age;
+	std::cout << "Enter your Age: ";
+	std::cin >> Age;
+
+	std::cout << "Your age is: " << Age << std::endl;
+
+	return 0;
+}
+*/
+
+
