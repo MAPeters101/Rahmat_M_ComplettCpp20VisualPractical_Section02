915800e3c026ba38c66c0139e86266adcf79da65
Mark Peters <mapeters101@earthlink.net>
2024-12-01 19:34:44 -0500

    Could not get lesson016 to work.  Issues with export/import a module.

---
+++ lesson016.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+import Math;
+
+extern int A;
+//int main()
+//{
+//	int Num1 = 39;
+//	int Num2 = 18;
+//
+//	std::cout << Num1 << " + " << Num2 << " = " << Add(Num1, Num2) << std::endl;
+//	std::cout << Num1 << " - " << Num2 << " = " << Subtract(Num1, Num2) << std::endl;
+//	std::cout << A;
+//	return 0;
+//}
+//
+//
+
+
+
+
+//#include <iostream>
+//
+//import Math;
+//
+//
+//int main() {
+//	
+//	std::cout << "13 + 15 = " << Add(13, 15) << "\n";
+//	std::cout << "13 - 15 = " << Subtract(13, 15) << "\n";
+//	
+//	return 0;
+//}
+
+
